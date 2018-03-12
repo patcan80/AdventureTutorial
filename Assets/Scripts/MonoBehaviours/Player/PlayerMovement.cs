641565e6cbd09382d8a0d293cab160692a84735b
Patrick Candler <patcan_80@yahoo.com>
2018-03-11 23:59:30 -0400

    Video 1

---
+++ Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public NavMeshAgent agent;
+
+
+}
